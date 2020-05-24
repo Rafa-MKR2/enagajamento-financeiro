e7bcc51465a01bfbc9af44fda1a57a7e0b5191f7
Rafael Do Carmo <rafaeldocarmo2010@gmail.com>
2020-05-24 17:21:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('./config/express'); var _express2 = _interopRequireDefault(_express);
 
-_express2.default.listen(3000, ()=>"Servidor rodando porta 3000");
+_express2.default.listen(8080, ()=>"Servidor rodando porta 3000");
